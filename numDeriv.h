3ba9d520a4eaaa98c454daf99fc2f07aaeaab62f
Maciej Bak <wsciekly.maciek@gmail.com>
2019-09-11 23:22:23 +0200

    updated modification date

--- numDeriv.h
+++ numDeriv.h
@@ -3,7 +3,7 @@
 //
 // Maciej Bak
 // Swiss Institute of Bioinformatics
-// 15.07.2019
+// 11.09.2019
 //
 
 // include guard:
@@ -11,20 +11,26 @@
 #ifndef NUMDERIV_H
 #define NUMDERIV_H
 
+// use C's linkage and naming for these functions
+// regardless of the compiler
+#ifdef __cplusplus
+extern "C" {
+#endif
 
 void gradient(double(*f)(unsigned short n_args, double* args, void* box), 
               unsigned short n_args, double* args, double* grad, void* box,
               double eps, double d, double zero_tol, unsigned short r, unsigned short v);
 
-
 void genD(double(*f)(unsigned short n_args, double* args, void* box), 
               unsigned short n_args, double* args, double* D, void* box,
               double eps, double d, double zero_tol, unsigned short r, unsigned short v);
-
 
 void hessian(double(*f)(unsigned short n_args, double* args, void* box), 
           unsigned short n_args, double* args, double* H, void* box,
           double eps, double d, double zero_tol, unsigned short r, unsigned short v);
 
+#ifdef __cplusplus
+}
+#endif
 
 #endif
